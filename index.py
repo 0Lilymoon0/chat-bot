22e054bb9d32c234d1834ea3152990bcf28c7bae
0Lilymoon0 <thelilcinnabun1@gmail.com>
2020-09-23 13:50:43 -0700

    Commit 2

--- index.py
+++ index.py
@@ -4,3 +4,9 @@ def get_math_bot_response(user_response):
 
   bot_response_correct = ["Correct!", "That's Right!", "You Got It!"]
   bot_response_incorrect = ["Incorrect", "Wrong", "Nope"]
+
+    if user_response == "10" or "ten" or "Ten":
+    return choice(bot_response_correct)
+  elif user_response !== "10" or "ten" or "Ten":
+    return choice(bot_response_incorrect)
+
