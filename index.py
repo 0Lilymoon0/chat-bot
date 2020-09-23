4059fbf33b4616e4a17cccdc6f7fea02e8a74ffc
0Lilymoon0 <thelilcinnabun1@gmail.com>
2020-09-23 15:06:42 -0700

    Edit 6

--- index.py
+++ index.py
@@ -2,23 +2,23 @@ from random import choice
 
 def get_math_bot_response(user_response):
 
-  bot_response_correct = ["Correct!", "That's Right!", "You Got It!"]
-  bot_response_incorrect = ["Incorrect", "Wrong", "Nope"]
+    bot_response_correct = ["Correct!", "That's Right!", "You Got It!"]
+    bot_response_incorrect = ["Incorrect", "Wrong", "Nope"]
 
-    if user_response == "10" or "ten" or "Ten":
-    return choice(bot_response_correct)
-  elif user_response !== "10" or "ten" or "Ten":
-    return choice(bot_response_incorrect)
+    if user_response == "10" or user_response == "ten" or user_response == "Ten":
+        return choice(bot_response_correct)
+    elif user_response != "10" and user_response != "ten" and user_response != "Ten":
+        return choice(bot_response_incorrect)
 
 print("Welcome to Math Bot")
-print("Please enter the answer to 5 + 5. If you get it right, go ahead and say 'done' the next time you are asked. ")
+print("You will be questioned on your math knowledge.")
 
 user_response = ""
 
 while True:
   user_response = input("Please enter the answer to 5 + 5. If you get it right, go ahead and say 'done' the next time you are asked. ")
 
-  if user_response == "done" or "Done":
+  if user_response == "done" or user_response == "Done":
     break
   
   bot_response = get_math_bot_response(user_response)
