8e29ab1ae137e0e2997376b233be0a2ff7389ee2
0Lilymoon0 <thelilcinnabun1@gmail.com>
2020-09-23 13:51:31 -0700

    Commit 3

--- index.py
+++ index.py
@@ -10,3 +10,8 @@ def get_math_bot_response(user_response):
   elif user_response !== "10" or "ten" or "Ten":
     return choice(bot_response_incorrect)
 
+print("Welcome to Math Bot")
+print("Please enter the answer to 5 + 5. If you get it right, go ahead and say 'done' the next time you are asked. ")
+
+user_response = ""
+
