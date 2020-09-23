15586252ab3ab8bca0b7f034e591e2f3754bb833
0Lilymoon0 <thelilcinnabun1@gmail.com>
2020-09-23 13:47:41 -0700

    Edit 1

---
+++ index.py
@@ -0,0 +1,6 @@
+from random import choice
+
+def get_math_bot_response(user_response):
+
+  bot_response_correct = ["Correct!", "That's Right!", "You Got It!"]
+  bot_response_incorrect = ["Incorrect", "Wrong", "Nope"]
